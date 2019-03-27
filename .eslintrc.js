349b392a38876841a512dc6e5208d9460c0ac090
at055612 <at055612@users.noreply.github.com>
2019-03-27 13:32:11 +0000

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: 'plugin:@typescript-eslint/recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,47 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { DragDropContext } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
-import Routes from "./startup/Routes";
-import useFontAwesome from "./startup/useFontAwesome";
+import Routes from './startup/Routes';
+import useFontAwesome from './startup/useFontAwesome';
 
-import { ThemeContextProvider } from "./lib/theme";
-import { CustomRouter } from "./lib/useRouter";
+import {ThemeContextProvider} from './lib/theme';
+import {CustomRouter} from './lib/useRouter';
 
-import createHistory from "history/createBrowserHistory";
-import { ConfigProvider } from "./startup/config";
+import createHistory from 'history/createBrowserHistory';
+import {ConfigProvider} from './startup/config';
 export const history = createHistory();
 
-import "react-table/react-table.css";
-import "./styles/main.css";
-import { AuthorisationContextProvider } from "./startup/Authorisation";
-import { AuthenticationContextProvider } from "./startup/Authentication";
-import { DocumentTreeContextProvider } from "./api/explorer";
-import { ErrorReportingContextProvider } from "./components/ErrorPage";
+import 'react-table/react-table.css';
+import './styles/main.css';
+import {AuthorisationContextProvider} from './startup/Authorisation';
+import {AuthenticationContextProvider} from './startup/Authentication';
+import {DocumentTreeContextProvider} from './api/explorer';
+import {ErrorReportingContextProvider} from './components/ErrorPage';
 
 const DndRoutes = DragDropContext(HTML5Backend)(Routes);
 
 const App: React.FunctionComponent = () => {
-  useFontAwesome();
-  return (
-    <ErrorReportingContextProvider>
-      <ConfigProvider>
-        <AuthenticationContextProvider>
-          <AuthorisationContextProvider>
-            <ThemeContextProvider>
-              <CustomRouter history={history}>
-                <DocumentTreeContextProvider>
-                  <DndRoutes />
-                </DocumentTreeContextProvider>
-              </CustomRouter>
-            </ThemeContextProvider>
-          </AuthorisationContextProvider>
-        </AuthenticationContextProvider>
-      </ConfigProvider>
-    </ErrorReportingContextProvider>
-  );
+    useFontAwesome();
+    return (
+        <ErrorReportingContextProvider>
+            <ConfigProvider>
+                <AuthenticationContextProvider>
+                    <AuthorisationContextProvider>
+                        <ThemeContextProvider>
+                            <CustomRouter history={history}>
+                                <DocumentTreeContextProvider>
+                                    <DndRoutes />
+                                </DocumentTreeContextProvider>
+                            </CustomRouter>
+                        </ThemeContextProvider>
+                    </AuthorisationContextProvider>
+                </AuthenticationContextProvider>
+            </ConfigProvider>
+        </ErrorReportingContextProvider>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
