3eef1a11f060ceed4d78810db04c5f1e8145b45f
at055612 <at055612@users.noreply.github.com>
2019-03-27 13:56:16 +0000

    Change bracketSpacing to true

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {DragDropContext} from 'react-dnd';
+import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 import Routes from './startup/Routes';
 import useFontAwesome from './startup/useFontAwesome';
 
-import {ThemeContextProvider} from './lib/theme';
-import {CustomRouter} from './lib/useRouter';
+import { ThemeContextProvider } from './lib/theme';
+import { CustomRouter } from './lib/useRouter';
 
 import createHistory from 'history/createBrowserHistory';
-import {ConfigProvider} from './startup/config';
+import { ConfigProvider } from './startup/config';
 export const history = createHistory();
 
 import 'react-table/react-table.css';
 import './styles/main.css';
-import {AuthorisationContextProvider} from './startup/Authorisation';
-import {AuthenticationContextProvider} from './startup/Authentication';
-import {DocumentTreeContextProvider} from './api/explorer';
-import {ErrorReportingContextProvider} from './components/ErrorPage';
+import { AuthorisationContextProvider } from './startup/Authorisation';
+import { AuthenticationContextProvider } from './startup/Authentication';
+import { DocumentTreeContextProvider } from './api/explorer';
+import { ErrorReportingContextProvider } from './components/ErrorPage';
 
 const DndRoutes = DragDropContext(HTML5Backend)(Routes);
 
