041e2a923f126568f66486475f1a6bd2f3a4645b
at055612 <at055612@users.noreply.github.com>
2019-04-03 16:58:05 +0100

    Fix eslint errors about html escaping

--- src/testing/New Developer/Step2_CallbacksAndState/PillChoice/PillChoice.stories.tsx
+++ src/testing/New Developer/Step2_CallbacksAndState/PillChoice/PillChoice.stories.tsx
@@ -12,13 +12,13 @@ addThemedStories(stories, () => {
     <div>
       <h2>Pill Choice Test Harness</h2>
       <p>
-        I often write a 'test harness' component which contains the simplest
-        possible usage of a specific component.
+        I often write a &apos;test harnes&apos; component which contains the
+        simplest possible usage of a specific component.
       </p>
       <p>
         This one passes a callback down to Pill Choice that spits the choice out
-        to the console. So now it's time to open the Chrome Developer tools and
-        observe the console when the buttons are clicked.
+        to the console. So now i&apos;s time to open the Chrome Developer tools
+        and observe the console when the buttons are clicked.
       </p>
       <PillChoice onChoice={p => console.log("Pill Chosen", p)} />
     </div>
