7a850637ecf691870bffddb031985272b56e401b
jc064522 <jc064522@gmail.com>
2019-05-08 10:49:28 +0100

    Move story file

--- src/components/tokens/Create/CreateToken.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { SearchToken } from "..";
-
-const stories = storiesOf("Tokens/Create", module);
-
-stories.add("basic", () => <SearchToken />);
