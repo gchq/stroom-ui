0a018d5e6069326e74cca6ce0f41cb4f9bb79922
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-16 13:01:56 +0100

    breaking up the data detail tabs thing, still working out what all the pieces are for

---
+++ src/components/DataViewer/DetailsTabs/StreamAttributes/StreamAttributes.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { DataRow } from "components/DataViewer/types";
+
+interface Props {
+  dataRow: DataRow;
+}
+
+const defaultAttributes = {
+  none: "none",
+};
+
+const StreamAttributes: React.FunctionComponent<Props> = ({ dataRow }) => (
+  <div className="tab-pane">
+    <div className="StreamDetails__container">
+      <div className="StreamDetails__table__container">
+        <table className="StreamDetails__table">
+          <tbody>
+            {Object.entries(dataRow.attributes || defaultAttributes)
+              .map(k => ({ key: k[0], value: k[1] }))
+              .map(({ key, value }) => {
+                if (key !== "Until" && key !== "Rule" && key !== "Age") {
+                  return (
+                    <tr>
+                      <td>{key}</td>
+                      <td>
+                        <code>{value}</code>
+                      </td>
+                    </tr>
+                  );
+                }
+                return undefined;
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+);
+
+export default StreamAttributes;
