834143557300afe449fc4eb0e1cfc7f0c7ffc86e
jc064522 <jc064522@gmail.com>
2019-07-15 10:03:56 +0100

    Fix dnd

--- src/components/IndexVolumes2/DraggableIndexVolumeCard.tsx
+++ src/components/IndexVolumes2/DraggableIndexVolumeCard.tsx
@@ -4,6 +4,8 @@ import DocRefImage from "../DocRefImage";
 import { IndexVolume } from "./indexVolumeApi";
 import InlineInput from "components/InlineInput/InlineInput";
 import { DraggableProvided, DraggableStateSnapshot } from "react-beautiful-dnd";
+import { render } from "react-dom";
+import { Component } from "react";
 
 interface Props {
   indexVolume: IndexVolume;
@@ -26,71 +28,74 @@ const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
   ...draggableStyle,
 });
 
-const DraggableIndexVolumeCard: React.FunctionComponent<Props> = ({
-  indexVolume,
-  provided,
-  snapshot,
-}) => {
-  var StyledCard = styled.div`
-    width: 24em;
-    cursor: grab;
-    margin-right: 1em;
-    border: 0.01em solid lightgrey;
-    padding: 0.7em;
-  `;
+var StyledCard = styled.div`
+  width: 24em;
+  cursor: grab;
+  margin-right: 1em;
+  border: 0.01em solid lightgrey;
+  padding: 0.7em;
+`;
 
-  const Field = styled.div`
-    display: flex;
-    flex-direction: row;
-    text-align: right;
-  `;
+const Field = styled.div`
+  display: flex;
+  flex-direction: row;
+  text-align: right;
+`;
 
-  const Label = styled.label`
-    width: 5.8em;
-    margin-right: 0.5em;
-  `;
+const Label = styled.label`
+  width: 5.8em;
+  margin-right: 0.5em;
+`;
 
-  const inlineInputWidth = 15;
+const inlineInputWidth = 15;
 
-  const Contents = styled.div`
-    display: flex;
-    flex-direction: row;
-  `;
+const Contents = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
-  const IconColumn = styled.div`
-    padding-right: 1em;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-  `;
+const IconColumn = styled.div`
+  padding-right: 1em;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
 
-  return (
-    <StyledCard
-      ref={provided.innerRef}
-      {...provided.draggableProps}
-      {...provided.dragHandleProps}
-      style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-    >
-      <Contents>
-        <IconColumn>
-          <DocRefImage docRefType="Index" size="lg" />
-        </IconColumn>
-        <div>
-          <Field>
-            <Label>Node name: </Label>
-            <InlineInput
-              value={indexVolume.nodeName}
-              width={inlineInputWidth}
-            />
-          </Field>
-          <Field>
-            <Label>Path: </Label>
-            <InlineInput value={indexVolume.path} width={inlineInputWidth} />
-          </Field>
-        </div>
-      </Contents>
-    </StyledCard>
-  );
-};
+/**
+ * This has to be a class component instead of a functional component
+ * because we need to use a 'ref' for BeautifulDnd to be able to handle drops.
+ */
+class DraggableIndexVolumeCard extends Component<Props> {
+  render() {
+    const { indexVolume, provided, snapshot } = this.props;
+    return (
+      <StyledCard
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+      >
+        <Contents>
+          <IconColumn>
+            <DocRefImage docRefType="Index" size="lg" />
+          </IconColumn>
+          <div>
+            <Field>
+              <Label>Node name: </Label>
+              <InlineInput
+                value={indexVolume.nodeName}
+                width={inlineInputWidth}
+              />
+            </Field>
+            <Field>
+              <Label>Path: </Label>
+              <InlineInput value={indexVolume.path} width={inlineInputWidth} />
+            </Field>
+          </div>
+        </Contents>
+      </StyledCard>
+    );
+  }
+}
 
 export default DraggableIndexVolumeCard;
