bbe1c29086bb5ecc753879bd2ab196606b29a0e7
at055612 <at055612@users.noreply.github.com>
2019-12-04 12:12:33 +0000

    Remove commented code

--- travis.script.sh
+++ travis.script.sh
@@ -166,9 +166,6 @@ main() {
     npm install
 
     echo -e "${GREEN}Running npm build${NC}"
-    # Set NODE_OPTIONS to try to prevent out of memory errors when 
-    # Both case styles as I am not sure which one is right
-    #NODE_OPTIONS="–max_old_space_size=8196 –max-old-space-size=8196" npm run build
     npm run build
 
     do_docker_build_if_required
